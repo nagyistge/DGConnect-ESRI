bbcdec081e727f381a77bbc648e199c196dbc74d
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-01-28 17:50:58 -0500

    UI Form and Button On Toolbar
    Added the UI form and the button that the user will click to search for a
    shapefile.

---
+++ GBDX/Utilities and Configuration/Forms/MappingForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gbdx.Utilities_and_Configuration.Forms
+{
+    public partial class MappingForm : Form
+    {
+        public MappingForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GBDX/VectorUpload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Gbdx
+{
+    public class VectorUpload : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public VectorUpload()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
