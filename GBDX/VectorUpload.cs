3bb56e079b08e065e9d8bc6c2d7a77b0360f41ff
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-02-09 17:17:35 -0500

    File dialog and zipping functionality
    Added the functionality to allow the user to browse the file structure and
    once the file has been chosen.  The mapping.properties file is created and
    then zipped.  I added an OpenSource project to handle the zip compression.

--- GBDX/VectorUpload.cs
+++ GBDX/VectorUpload.cs
@@ -6,6 +6,10 @@ using System.IO;
 
 namespace Gbdx
 {
+    using System.Windows.Forms;
+
+    using Ionic.Zip;
+
     public class VectorUpload : ESRI.ArcGIS.Desktop.AddIns.Button
     {
         public VectorUpload()
@@ -14,6 +18,52 @@ namespace Gbdx
 
         protected override void OnClick()
         {
+            try
+            {
+                var openFileDialog = new OpenFileDialog() { Multiselect = false, };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var directoryPath = Path.GetDirectoryName(openFileDialog.FileName);
+
+                    var newZip = directoryPath + "\\"
+                                       + (long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds+".zip";
+
+                    string mappingProps = "mapping.properties";
+                    const string UserContributions = "User Contributions";
+                    string itemType = "";
+                    string spatialReference = "";
+
+                    // Write the mapping.properties file.
+                    if (!File.Exists(mappingProps))
+                    {
+                        using (var sw = File.CreateText(mappingProps))
+                        {
+                            sw.WriteLine("vector.crs={0}", spatialReference);
+                            sw.WriteLine("vector.ingestSource={0}", UserContributions);
+                            sw.WriteLine("vector.itemType={0}", itemType);
+                            sw.WriteLine("vector.index=vector-{0}-{1}-{2}",UserContributions,itemType , DateTime.Now.ToString("yyyy-MM-dd'T'HH:mm:ss.fff'Z'"));
+                            sw.WriteLine("tagger_id=source");
+                            sw.WriteLine("id=name");
+                            sw.Flush();
+                            sw.Close();
+                        }
+                    }
+
+                    using (var zip = new ZipFile())
+                    {
+                        zip.AddFile(openFileDialog.FileName,"");
+                        zip.AddFile(mappingProps);
+                        zip.Save(newZip);
+                    }
+
+                    File.Delete(mappingProps);
+                }
+            }
+            catch (Exception error)
+            {
+                
+            }
         }
 
         protected override void OnUpdate()
