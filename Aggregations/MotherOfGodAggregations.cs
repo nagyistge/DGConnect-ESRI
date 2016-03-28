fb5772e3fc2541f131242af0f976d93f713de779
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-03-28 15:14:06 -0400

    Aggregation Polygon AOI functionality
    The aggregation fuctionality has been updated to be capable of using
    selected polygons as the search AOI

--- Aggregations/MotherOfGodAggregations.cs
+++ Aggregations/MotherOfGodAggregations.cs
@@ -6,7 +6,7 @@
     {
         public string responseDate { get; set; }
         public Geom geom { get; set; }
-        public string query { get; set; }
+        public object query { get; set; }
         public object startDate { get; set; }
         public object endDate { get; set; }
         public int totalItems { get; set; }
