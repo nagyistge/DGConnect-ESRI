9d7c37b7f53be0c1b510c695f9e09b23d79baf36
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-04-13 13:52:08 -0400

    Corrected issue with wrong class stubbed out

--- GBDX/Answer Factory/AnswerFactoryButton.cs
+++ GBDX/Answer Factory/AnswerFactoryButton.cs
@@ -6,6 +6,8 @@ using System.IO;
 
 namespace Gbdx
 {
+    using ESRI.ArcGIS.esriSystem;
+
     public class AnswerFactoryButton : ESRI.ArcGIS.Desktop.AddIns.Button
     {
         public AnswerFactoryButton()
@@ -14,6 +16,10 @@ namespace Gbdx
 
         protected override void OnClick()
         {
+            UID theUid = new UIDClass();
+            theUid.Value = ThisAddIn.IDs.Gbdx_Answer_Factory_AnswerFactoryDockableWindow;
+            var window = ArcMap.DockableWindowManager.GetDockableWindow(theUid);
+            window.Show(!window.IsVisible());
         }
 
         protected override void OnUpdate()
