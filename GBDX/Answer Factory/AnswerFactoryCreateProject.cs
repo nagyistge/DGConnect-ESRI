4106769ce1e63c32bc3fd0deebec1c4435f6848e
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-04-13 13:22:07 -0400

    AnswerFactory project structure stub

---
+++ GBDX/Answer Factory/AnswerFactoryCreateProject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Gbdx
+{
+    public class AnswerFactoryCreateProject : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public AnswerFactoryCreateProject()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
---
+++ GBDX/Answer Factory/AnswerFactoryProjects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Gbdx
+{
+    public class AnswerFactoryButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public AnswerFactoryButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
