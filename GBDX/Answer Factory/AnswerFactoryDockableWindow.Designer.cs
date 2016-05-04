ce98ade266b21d59fd7e67a62d0e9f82427cce43
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-05-04 15:02:48 -0400

    Initial AnswerFactory UI v2
    No functionality established ... in fact most things will probably break
    at this point.

--- GBDX/Answer Factory/AnswerFactoryDockableWindow.Designer.cs
+++ GBDX/Answer Factory/AnswerFactoryDockableWindow.Designer.cs
@@ -41,19 +41,17 @@
             this.drawButton = new System.Windows.Forms.Button();
             this.saveButton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.existingProjectsListView = new System.Windows.Forms.ListView();
-            this.nameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.recipeHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.statusHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.idHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.showResultsButton = new System.Windows.Forms.Button();
             this.resultRefrshButton = new System.Windows.Forms.Button();
+            this.showResultsButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.projectSearchTextBox = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -120,7 +118,7 @@
             // resetButton
             // 
             this.resetButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.resetButton.Location = new System.Drawing.Point(46, 112);
+            this.resetButton.Location = new System.Drawing.Point(25, 112);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(65, 30);
             this.resetButton.TabIndex = 10;
@@ -133,9 +131,9 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 8);
+            this.label1.Location = new System.Drawing.Point(29, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.Size = new System.Drawing.Size(83, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Project Name";
             // 
@@ -143,9 +141,9 @@
             // 
             this.projectNameTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel2.SetColumnSpan(this.projectNameTextbox, 2);
-            this.projectNameTextbox.Location = new System.Drawing.Point(160, 5);
+            this.projectNameTextbox.Location = new System.Drawing.Point(118, 8);
             this.projectNameTextbox.Name = "projectNameTextbox";
-            this.projectNameTextbox.Size = new System.Drawing.Size(214, 26);
+            this.projectNameTextbox.Size = new System.Drawing.Size(256, 20);
             this.projectNameTextbox.TabIndex = 7;
             // 
             // label3
@@ -153,9 +151,9 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 44);
+            this.label3.Location = new System.Drawing.Point(3, 47);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.Size = new System.Drawing.Size(109, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Available Recipes";
             // 
@@ -163,9 +161,9 @@
             // 
             this.availableRecipesCombobox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.availableRecipesCombobox.FormattingEnabled = true;
-            this.availableRecipesCombobox.Location = new System.Drawing.Point(160, 40);
+            this.availableRecipesCombobox.Location = new System.Drawing.Point(118, 43);
             this.availableRecipesCombobox.Name = "availableRecipesCombobox";
-            this.availableRecipesCombobox.Size = new System.Drawing.Size(135, 28);
+            this.availableRecipesCombobox.Size = new System.Drawing.Size(177, 21);
             this.availableRecipesCombobox.TabIndex = 9;
             // 
             // label2
@@ -173,9 +171,9 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(15, 80);
+            this.label2.Location = new System.Drawing.Point(11, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.Size = new System.Drawing.Size(92, 13);
             this.label2.TabIndex = 11;
             this.label2.Text = "Selection Type";
             // 
@@ -186,9 +184,9 @@
             this.selectionTypecomboBox.Items.AddRange(new object[] {
             "Draw Rectangle",
             "Use Selected Feature"});
-            this.selectionTypecomboBox.Location = new System.Drawing.Point(160, 76);
+            this.selectionTypecomboBox.Location = new System.Drawing.Point(118, 79);
             this.selectionTypecomboBox.Name = "selectionTypecomboBox";
-            this.selectionTypecomboBox.Size = new System.Drawing.Size(135, 28);
+            this.selectionTypecomboBox.Size = new System.Drawing.Size(177, 21);
             this.selectionTypecomboBox.TabIndex = 12;
             this.selectionTypecomboBox.SelectedIndexChanged += new System.EventHandler(this.selectionTypecomboBox_SelectedIndexChanged);
             // 
@@ -226,86 +224,7 @@
             this.groupBox2.Size = new System.Drawing.Size(389, 376);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Open Projects";
-            // 
-            // tableLayoutPanel3
-            // 
-            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel3.ColumnCount = 3;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.99999F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.00001F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel3.Controls.Add(this.existingProjectsListView, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.showResultsButton, 2, 4);
-            this.tableLayoutPanel3.Controls.Add(this.resultRefrshButton, 0, 4);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(12, 21);
-            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 5;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(371, 349);
-            this.tableLayoutPanel3.TabIndex = 0;
-            // 
-            // existingProjectsListView
-            // 
-            this.existingProjectsListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.existingProjectsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.nameHeader,
-            this.recipeHeader,
-            this.statusHeader,
-            this.idHeader});
-            this.tableLayoutPanel3.SetColumnSpan(this.existingProjectsListView, 3);
-            this.existingProjectsListView.FullRowSelect = true;
-            this.existingProjectsListView.GridLines = true;
-            this.existingProjectsListView.Location = new System.Drawing.Point(3, 3);
-            this.existingProjectsListView.MultiSelect = false;
-            this.existingProjectsListView.Name = "existingProjectsListView";
-            this.tableLayoutPanel3.SetRowSpan(this.existingProjectsListView, 4);
-            this.existingProjectsListView.Size = new System.Drawing.Size(365, 302);
-            this.existingProjectsListView.TabIndex = 0;
-            this.existingProjectsListView.UseCompatibleStateImageBehavior = false;
-            this.existingProjectsListView.View = System.Windows.Forms.View.Details;
-            this.existingProjectsListView.SelectedIndexChanged += new System.EventHandler(this.existingProjectsListView_SelectedIndexChanged);
-            // 
-            // nameHeader
-            // 
-            this.nameHeader.Text = "Name";
-            this.nameHeader.Width = 125;
-            // 
-            // recipeHeader
-            // 
-            this.recipeHeader.Text = "Recipe";
-            this.recipeHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.recipeHeader.Width = 163;
-            // 
-            // statusHeader
-            // 
-            this.statusHeader.Text = "Status";
-            this.statusHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.statusHeader.Width = 72;
-            // 
-            // idHeader
-            // 
-            this.idHeader.Text = "ID";
-            this.idHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // showResultsButton
-            // 
-            this.showResultsButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.showResultsButton.Location = new System.Drawing.Point(263, 312);
-            this.showResultsButton.Name = "showResultsButton";
-            this.showResultsButton.Size = new System.Drawing.Size(104, 33);
-            this.showResultsButton.TabIndex = 1;
-            this.showResultsButton.Text = "Show Results";
-            this.showResultsButton.UseVisualStyleBackColor = true;
-            this.showResultsButton.Click += new System.EventHandler(this.showResultsButton_Click);
+            this.groupBox2.Text = "Projects";
             // 
             // resultRefrshButton
             // 
@@ -318,6 +237,63 @@
             this.resultRefrshButton.UseVisualStyleBackColor = true;
             this.resultRefrshButton.Click += new System.EventHandler(this.resultRefrshButton_Click);
             // 
+            // showResultsButton
+            // 
+            this.showResultsButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.showResultsButton.Location = new System.Drawing.Point(263, 312);
+            this.showResultsButton.Name = "showResultsButton";
+            this.showResultsButton.Size = new System.Drawing.Size(104, 33);
+            this.showResultsButton.TabIndex = 1;
+            this.showResultsButton.Text = "Show Results";
+            this.showResultsButton.UseVisualStyleBackColor = true;
+            this.showResultsButton.Click += new System.EventHandler(this.showResultsButton_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.99999F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.00001F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel3.Controls.Add(this.showResultsButton, 2, 4);
+            this.tableLayoutPanel3.Controls.Add(this.resultRefrshButton, 0, 4);
+            this.tableLayoutPanel3.Controls.Add(this.splitContainer1, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.projectSearchTextBox, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(12, 21);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.792208F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.20779F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(371, 349);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.SetColumnSpan(this.splitContainer1, 3);
+            this.splitContainer1.Location = new System.Drawing.Point(3, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            this.tableLayoutPanel3.SetRowSpan(this.splitContainer1, 3);
+            this.splitContainer1.Size = new System.Drawing.Size(365, 278);
+            this.splitContainer1.SplitterDistance = 126;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // projectSearchTextBox
+            // 
+            this.projectSearchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.projectSearchTextBox.Location = new System.Drawing.Point(3, 3);
+            this.projectSearchTextBox.Name = "projectSearchTextBox";
+            this.projectSearchTextBox.Size = new System.Drawing.Size(124, 20);
+            this.projectSearchTextBox.TabIndex = 4;
+            // 
             // AnswerFactoryDockableWindow
             // 
             this.Controls.Add(this.tableLayoutPanel1);
@@ -329,6 +305,8 @@
             this.tableLayoutPanel2.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -344,18 +322,15 @@
         private System.Windows.Forms.TextBox projectNameTextbox;
         private System.Windows.Forms.ComboBox availableRecipesCombobox;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.ListView existingProjectsListView;
-        private System.Windows.Forms.Button showResultsButton;
-        private System.Windows.Forms.ColumnHeader nameHeader;
-        private System.Windows.Forms.ColumnHeader recipeHeader;
-        private System.Windows.Forms.Button resultRefrshButton;
         private System.Windows.Forms.Button resetButton;
-        private System.Windows.Forms.ColumnHeader statusHeader;
-        private System.Windows.Forms.ColumnHeader idHeader;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox selectionTypecomboBox;
         private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button showResultsButton;
+        private System.Windows.Forms.Button resultRefrshButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox projectSearchTextBox;
 
     }
 }
