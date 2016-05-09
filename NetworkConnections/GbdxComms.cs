2e02741821a70b0a9aa82897c1071585b849400f
Russ Wittmer <Russell.Wittmer@digitalglobe.com>
2016-05-09 17:16:16 -0400

    Removed Unused References From All Projects
    Removed unused references from all proijects

--- NetworkConnections/GbdxComms.cs
+++ NetworkConnections/GbdxComms.cs
@@ -25,12 +25,9 @@ namespace NetworkConnections
     using System.Collections.Generic;
     using System.Collections.Specialized;
     using System.IO;
-    using System.Linq;
     using System.Net;
     using System.Net.Security;
     using System.Text;
-    using System.Web.SessionState;
-
     using GbdxTools;
 
     using Logging;
