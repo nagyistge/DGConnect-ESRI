3730bc4b69ff8f5baaccb73c85f2497ff48baed8
Russ Wittmer <russell.wittmer@digitalglobe.com>
2016-06-14 17:00:44 -0400

    Updated Project2 Class and Added Unit Tests
    Updated Project2 so recipes created via the webapp will now properly
    deserialize.  In finding what needed to be changed i created a new unit
    test project for answer factory.

---
+++ Answer Factory Unit Tests/ObjectDeserialization.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace Answer_Factory_Unit_Tests
+{
+    using AnswerFactory;
+
+    [TestClass]
+    public class ObjectDeserialization
+    {
+        private static JsonDeserializer deserial = new JsonDeserializer();
+        [TestMethod]
+        public void Project2Deserialization()
+        {
+            //try
+            //{
+                string content = TestData.Project2JsonData;
+                var result =
+                    deserial.Deserialize<Project2>(new RestResponse<Project2> { Content = content });
+
+                if (result == null)
+                {
+                    Assert.Fail();
+                }
+            //}
+            //catch (Exception error)
+            //{
+            //    Assert.Fail();
+            //}
+        }
+    }
+}
