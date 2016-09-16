5e6996ba2f90af1ef2b86d0f6096ae995213ae6a
Russ Wittmer <wittmer85@gmail.com>
2016-09-16 19:35:41 -0400

    Working commit

--- GBDX/CatalogTokenRefresh.cs
+++ GBDX/CatalogTokenRefresh.cs
@@ -49,6 +49,7 @@ namespace Gbdx
 
             if (!working)
             {
+                var errorOccurred = false;
                 working = true;
 
                 try
@@ -58,16 +59,20 @@ namespace Gbdx
                         GetToken();
                     }
 
-                    UpdateTokens(ArcMap.Document.FocusMap);
+                    UpdateTokens2(ArcMap.Document.FocusMap);
                 }
                 catch (Exception error)
                 {
+                    errorOccurred = true;
                     Jarvis.Logger.Error(error);
                 }
                 finally
                 {
-                    MessageBox.Show("Layer refresh complete");
                     working = false;
+                }
+                if(!errorOccurred)
+                {
+                    MessageBox.Show("Layer refresh complete");
                 }
             }
         }
@@ -112,20 +117,47 @@ namespace Gbdx
             }
         }
 
+        private static void UpdateTokens2(IMap map)
+        {
+            for (var i = 0; i < map.LayerCount; i++)
+            {
+                ReplaceIdahoIds(null, map.Layer[i], 0);
+            }
+
+        }
+
         private static void UpdateTokens(IMap map)
         {
             var catIDictionary = CheckLayers(map);
-
+            bool error = false;
 
             List<IGroupLayer> grpLayers = new List<IGroupLayer>();
+
+            // Don't remove layers in this loop because if there is an error we don't want to take data away from the users that wouldn't be easily recoverable from.
             foreach (var key in catIDictionary.Keys)
             {
-                //RemoveLayer(key);
+                var tempLayer = RefreshLayer(key, catIDictionary[key], out error);
 
-                grpLayers.Add(RefreshLayer(key, catIDictionary[key]));
-
+                if (error)
+                {
+                    MessageBox.Show("An error occurred while trying to refresh the WMS layers. \n Due to this error NO changes were made.\n  Please try again.");
+                    throw new Exception("An error occurred while attempting to refresh WMS");
+                }
+                grpLayers.Add(tempLayer);
             }
 
+            // remove the existing layers now that we have a suitable replacment for all
+            foreach (var key in catIDictionary.Keys)
+            {
+                RemoveLayer(key);
+            }
+
+            // add the refreshed wms back to the mxd
+            foreach (var layer in grpLayers)
+            {
+                ArcMap.Document.AddLayer(layer);
+                ArcMap.Document.FocusMap.MoveLayer(layer, 0);
+            }
         }
 
         private static WMSMapLayerClass CreateWmsMapLayer(string id, int attempt = 0)
@@ -172,7 +204,7 @@ namespace Gbdx
             return wmsMapLayer;
         }
 
-        private static IGroupLayer RefreshLayer(string catId, List<string> idahoIds)
+        private static IGroupLayer RefreshLayer(string catId, List<string> idahoIds, out bool unRecoverableError)
         {
             IGroupLayer groupLayer = new GroupLayerClass();
             groupLayer.Name = catId;
@@ -182,40 +214,10 @@ namespace Gbdx
 
                 if(wmsMapLayer == null)
                 {
-                    Jarvis.Logger.Info("something bad happened");
-                    continue;
+                    Jarvis.Logger.Info("An unknown issue occurred trying to establish a WMS layer link");
+                    unRecoverableError = true;
+                    return null;
                 }
-                //// create and configure wms connection name, this is used to store the connection properties
-                //IWMSConnectionName pConnName = new WMSConnectionNameClass();
-                //IPropertySet propSet = new PropertySetClass();
-
-                //// create the idaho wms url
-                //var idahoUrl = string.Format(
-                //    "http://idaho.geobigdata.io/v1/wms/idaho-images/{0}/{1}/mapserv?",
-                //    id,
-                //    token);
-
-                //Jarvis.Logger.Info("Adding WMS Layer to: " + idahoUrl);
-
-                //// setup the arcmap connection properties
-                //propSet.SetProperty("URL", idahoUrl);
-                //pConnName.ConnectionProperties = propSet;
-
-                ////uses the name information to connect to the service
-                //IDataLayer dataLayer = wmsMapLayer;
-                //try
-                //{
-                //    dataLayer.Connect((IName)pConnName);
-                //}
-                //catch (Exception e)
-                //{
-                //    Jarvis.Logger.Error("Problems connecting to WMS: " + e.Message);
-                //    if (attempts <= 5)
-                //    {
-                //        RefreshLayer(catId,idahoIds,token, attempts++);
-                //    }
-                //    return;
-                //}
 
                 // get wms service description
                 var serviceDesc = wmsMapLayer.IWMSGroupLayer_WMSServiceDescription;
@@ -239,9 +241,9 @@ namespace Gbdx
                 SublayerVisibleOn(wmsLayer);
                 groupLayer.Add(wmsLayer);
             }
-            // turn on sub layers, add it to arcmap and move it to top of TOC
-        //    ArcMap.Document.AddLayer(groupLayer);
-        //    ArcMap.Document.FocusMap.MoveLayer(groupLayer, 0);
+
+            // No error occurred
+            unRecoverableError = false;
             return groupLayer;
         }
 
@@ -302,6 +304,53 @@ namespace Gbdx
             return catIDictionary;
         }
 
+        private static Dictionary<string, List<WmsLayerInfo>> CheckLayers2(IMap map)
+        {
+            Dictionary<string, List<WmsLayerInfo>> output = new Dictionary<string, List<WmsLayerInfo>>();
+
+            for (int i = 0; i < map.LayerCount; i++)
+            {
+                var layer = map.Layer[i];
+
+                var compLayer = (ICompositeLayer) layer;
+
+                // if composite layer has more than 1 layer check the sub layers
+                if (compLayer.Count > 1)
+                {
+                    //CheckSubLayers2(compLayer);
+                }
+
+            }
+            return output;
+        }
+
+        private static List<WmsLayerInfo> CheckSubLayers2(ILayer layer, Dictionary<string, List<WmsLayerInfo>> layerDictionary)
+        {
+            var output = new List<WmsLayerInfo>();
+            var tempLayer = (ICompositeLayer) layer;
+
+            if (tempLayer.Count > 1)
+            {
+                for (int i = 0; i < tempLayer.Count; i++)
+                {
+                    var temp = tempLayer.Layer[i];
+                    if (IdahoIdPattern.IsMatch(temp.Name))
+                    {
+                        output.Add(new WmsLayerInfo(i, temp.Name));
+                    }
+                }
+
+            layerDictionary.Add(layer.Name, output);
+            }
+            else
+            {
+                
+            }
+            
+
+            return output;
+        }
+
         /// <summary>
         /// Search for and delete provided layer.
         /// </summary>
@@ -350,6 +399,163 @@ namespace Gbdx
                 }
             }
             return output;
+        }
+
+
+        private static int FindLayer(ILayer layer)
+        {
+            for (int i = 0; i < ArcMap.Document.FocusMap.LayerCount; i++)
+            {
+                var templayer = ArcMap.Document.FocusMap.Layer[i];
+                if (templayer.Name != layer.Name)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private static void ReplaceIdahoIds(ILayer parentLayer, ILayer childLayer, int count)
+        {
+            // idaho id layer name found.  so let's swap it out
+            if (IdahoIdPattern.IsMatch(childLayer.Name))
+            {
+                IGroupLayer refreshedGroupLayer = null;
+
+                if (parentLayer == null)
+                {
+                    refreshedGroupLayer = Refreshlayers(childLayer);
+                    var index = FindLayer(childLayer);
+                    ArcMap.Document.FocusMap.DeleteLayer(childLayer);
+                    ArcMap.Document.FocusMap.MoveLayer(refreshedGroupLayer,index);
+                }
+                else
+                {
+                    refreshedGroupLayer = Refreshlayers(parentLayer);
+                    var index = FindLayer(parentLayer);
+                    ArcMap.Document.FocusMap.DeleteLayer(parentLayer);
+                    ArcMap.Document.FocusMap.MoveLayer(refreshedGroupLayer, index);
+                }
+
+                if (refreshedGroupLayer == null)
+                {
+                    return;
+                }
+                
+                ArcMap.Document.FocusMap.AddLayer(refreshedGroupLayer);
+
+                return;
+            }
+
+            var childCompLayer = childLayer as ICompositeLayer;
+
+            // check to see if the child has any children
+            if (childCompLayer != null && childCompLayer.Count > 1)
+            {
+                // check children layers of current child
+                ReplaceIdahoIds(childLayer, childCompLayer.Layer[0], 0);
+            }
+            else
+            {
+                // Keep checking the other layers of the parent layer
+                var compLayer = (ICompositeLayer) parentLayer;
+                if (count + 1 < compLayer.Count)
+                {
+                    ReplaceIdahoIds(parentLayer, compLayer.Layer[count + 1], count + 1);
+                }
+            }
+        }
+
+        private static ILayer RefreshIdahoId(string idahoID)
+        {
+
+            var wmsMapLayer = CreateWmsMapLayer(idahoID);
+
+            if (wmsMapLayer == null)
+            {
+                return null;
+            }
+
+            var serviceDesc = wmsMapLayer.IWMSGroupLayer_WMSServiceDescription;
+            ILayer wmsLayer = null;
+
+            // add layers for the wms currently there will only be one
+            for (var i = 0; i <= serviceDesc.LayerDescriptionCount - 1; i++)
+            {
+                var layerDesc = serviceDesc.LayerDescription[i];
+
+                var grpLayer = wmsMapLayer.CreateWMSGroupLayers(layerDesc);
+                for (var j = 0; j <= grpLayer.Count - 1; j++)
+                {
+                    wmsLayer = wmsMapLayer;
+                    wmsMapLayer.Name = idahoID;
+                }
+            }
+
+            SublayerVisibleOn(wmsLayer);
+            return wmsLayer;
+        }
+        
+        private static List<ILayer> GetLayers(ILayer parent)
+        {
+            var compLayer = parent as ICompositeLayer2;
+            var output = new List<ILayer>();
+
+            if (compLayer == null)
+            {
+                return output;
+            }
+
+            for (int i = 0; i <= compLayer.Count; i++)
+            {
+                output.Add(compLayer.Layer[i]);
+            }
+
+            return output;
+        }
+
+        private static IGroupLayer Refreshlayers(ILayer parent)
+        {
+            var compLayer = parent as ICompositeLayer2;
+            List<ILayer> layerList;
+            if (compLayer!= null && compLayer.Count > 1)
+            {
+                layerList = GetLayers(parent);
+
+                // go through the layers and refresh them if they are idaho id named layers
+                for (int i = 0; i < layerList.Count; i++)
+                {
+                    var layer = layerList[i];
+                    if (IdahoIdPattern.IsMatch(layer.Name))
+                    {
+                        layerList[i] = RefreshIdahoId(layer.Name);
+                    }
+                }
+
+                IGroupLayer groupLayer = new GroupLayerClass();
+                groupLayer.Name = parent.Name;
+
+                for (var j = 0; j < layerList.Count; j++)
+                {
+                    groupLayer.Add(layerList[j]);
+                }
+
+                return groupLayer;
+            }
+
+            return null;
+        }
+    }
+
+    public class WmsLayerInfo
+    {
+        public int LayerIndex { get; set; }
+        public string IdahoId { get; set; }
+
+        public WmsLayerInfo(int index, string id)
+        {
+            this.LayerIndex = index;
+            this.IdahoId = id;
         }
     }
 }
